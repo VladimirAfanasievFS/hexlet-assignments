e150adfc7fe6e4d10ad551d595a9596232131c8d
@hexlet/cli <support@hexlet.io>
2023-07-08 16:05:11 +0300

    submit rails-basics-ru/quality

--- rails-basics-ru/quality/app/controllers/bulletins_controller.rb
+++ rails-basics-ru/quality/app/controllers/bulletins_controller.rb
@@ -2,6 +2,13 @@
 
 class BulletinsController < ApplicationController
   # BEGIN
-  
+  def index
+    @bulletins = Bulletin.all
+    pp @bulletins
+  end
+
+  def show
+    @bulletin = Bulletin.find(params[:id])
+  end
   # END
 end
---
+++ rails-basics-ru/quality/test/integration/bullets_flow_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class BulletsFlowTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'opens all bulletins page' do
+    get bulletins_path
+    assert_response :success
+    assert_select 'h1', 'Bulletins'
+  end
+
+  test 'opens one note page with fixture' do
+    bullet1 = bulletins(:bullet1)
+    get bulletin_path(bullet1)
+    assert_response :success
+    assert_select 'p1', bullet1.title
+    assert_select 'p1', bullet1.body
+  end
+end
