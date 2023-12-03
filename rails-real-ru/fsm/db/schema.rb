0d3bacd93280e319431ef8a8f8e21d0322369174
@hexlet/cli <support@hexlet.io>
2023-12-03 23:26:56 +0300

    submit rails-real-ru/fsm

--- rails-real-ru/fsm/db/schema.rb
+++ rails-real-ru/fsm/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_09_19_155610) do
+ActiveRecord::Schema[6.1].define(version: 2022_09_19_155610) do
   create_table "vacancies", force: :cascade do |t|
     t.string "title"
     t.string "description"
